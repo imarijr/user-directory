3f27a9ffb52eb49259dd3160c81fc9fc1f1f19de
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-15 15:16:06 -0400

    Focus userName on page load and after each submit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const handleSubmit = function(ev){
     const userName = f.userName.value
     const age = f.age.value
     users.innerHTML += '<p>' + userName + ', ' + age + '<p>' 
+   
     f.reset()
+    f.userName.focus()
 }
 
 
