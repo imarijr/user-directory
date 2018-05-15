33b1a3adb4b375d7dced425ea3206b055fd9cce9
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-14 23:17:19 -0400

    Change second heading text when button is clicked

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const button = document.querySelector('button.greeting')
+const button = document.querySelector('button.button1')
 
 
 function changeText()
 {
-    const p = document.querySelector('h2.heading2')
-    p.textContent ='WOAH!'
- }
+    const p = document.querySelector('h1.heading2')
+    p.textContent ='WOAH'
+}
 
 button.addEventListener('click', changeText)
+
+ 
