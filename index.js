e2b0032b7c9ae4450d76f4b46c071d3c301bb255
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-15 10:18:59 -0400

    change heading to input on button click

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const button = document.querySelector('button.button1')
 
 function changeText()
 {
-    const p = document.querySelector('h1.heading2')
-    p.textContent ='WOAH'
+    const p = document.querySelector('h2.heading2')
+    p.textContent = document.getElementById("newTitle").value;
 }
 
 button.addEventListener('click', changeText)
 
- 
