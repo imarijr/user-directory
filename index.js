990b6c68528c800a85924e572f339e50ad9e6164
Imari McKinney Jr <imarijr7@gmail.com>
2018-05-15 16:23:20 -0400

    Add color div to each users list

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ const form=document.querySelector('#userForm')
 
 const handleSubmit = function(ev){
     ev.preventDefault()
-    const users =document.querySelector('#users')
+    
     const f = ev.target
     const userName = f.userName.value
     const age = f.age.value
     const color = f.favoriteColor.value
     
-    const list = document.createElement('ul')
+    
     const nameItem = document.createElement('li')
     nameItem.textContent = `Name: ${userName}`
 
     const ageItem =  document.createElement('li')
     ageItem.textContent = `Age: ${age}`
 
+    const colorItem =  document.createElement('li')
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+    colorItem.appendChild(colorDiv)
+
     
 
-    users.appendChild(list)
+    const list = document.createElement('ul')
     list.appendChild(nameItem)
     list.appendChild(ageItem)
+    list.appendChild(colorItem)
 
-   
+
+   const users =document.querySelector('#users')
+    users.appendChild(list)
     f.reset()
     f.userName.focus()
 }
